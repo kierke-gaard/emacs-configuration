fefdcc3c601928a6de848e46a1f64665b90cd4c2
j-clausen <j.clausen@kigroup.de>
2020-05-26 10:50:30 +0200

    add keybinding string rectangle and copy-line function

--- init.el
+++ init.el
@@ -49,6 +49,28 @@
 (require 'helm-projectile)
 (global-set-key (kbd "C-;") 'comment-line)
 
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+	(end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+	  (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+	(setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+	(kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "C-)") 'paredit-forward-slurp-sexp) ;sollte auch ohne das gehen!
@@ -197,8 +219,7 @@
 
 (global-set-key [(meta up)]  'move-line-up)
 (global-set-key [(meta down)]  'move-line-down)
-
-
+(global-set-key (kbd "C-x r e") 'string-insert-rectangle)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (scroll-bar-mode -1)
