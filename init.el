2df8e85f9677c9cb9d8e8f2dae15a4529ad31a35
j-clausen <j.clausen@kigroup.de>
2020-12-21 12:19:34 +0100

    cleanup

--- init.el
+++ init.el
@@ -4,12 +4,8 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
-;; server mode for emacsclient
 (server-start)
 
-;; Automatische Dateibackups sollen in ein eigenes Verzeichnis.  Per
-;; Default wird sonst neben jeder editierten Datei der letzte vorige
-;; Zustand mit angehängter Tilde am Dateinamen abgelegt.
 (setq backup-by-copying t
       backup-directory-alist '(("." . "~/.saves")) ; don't litter my fs tree
       delete-old-versions t
@@ -28,59 +24,28 @@
 (setq-default fill-column 80)
 (display-battery-mode t)
 (setq inhibit-startup-message t)
-
-;; Whitespace-mode (M-x whitespace-mode) zeigt
-;; Whitespace-Unschönheiten an.  Tip: Spaces am Zeilenende mit M-x
-;; delete-trailing-whitespace entfernen.
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (setq-default whitespace-line-column 80)
 
-;; Auto save files to user-emacs-directory
 (setq auto-save-file-name-transforms
           `((".*" ,(concat user-emacs-directory "auto-save/") t)))
 
 
-;; M-x package-install-selected-packages, anschließend die folgenden Blöcke
-;; ent-kommentieren.
 (require 'sql)
 
 (require 'helm)
 (require 'helm-config)
 (require 'helm-projectile)
 (global-set-key (kbd "C-;") 'comment-line)
-
-(defun copy-line (arg)
-  "Copy lines (as many as prefix argument) in the kill ring.
-      Ease of use features:
-      - Move to start of next line.
-      - Appends the copy on sequential calls.
-      - Use newline as last char even on the last line of the buffer.
-      - If region is active, copy its lines."
-  (interactive "p")
-  (let ((beg (line-beginning-position))
-	(end (line-end-position arg)))
-    (when mark-active
-      (if (> (point) (mark))
-	  (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-	(setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-    (if (eq last-command 'copy-line)
-	(kill-append (buffer-substring beg end) (< end beg))
-      (kill-ring-save beg end)))
-  (kill-append "\n" nil)
-  (beginning-of-line (or (and arg (1+ arg)) 2))
-  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
-
-
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "C-)") 'paredit-forward-slurp-sexp) ;sollte auch ohne das gehen!
 (global-unset-key (kbd "C-x c"))
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-z") 'helm-select-action)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(setq helm-split-window-in-side-p t ; open helm buffer in current window, do not occupy whole other window
-      helm-move-to-line-cycle-in-source t ; move to end or beginning of buffer when reaching top or bottom
+(setq helm-split-window-in-side-p t
+      helm-move-to-line-cycle-in-source t
       helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp
       helm-scroll-amount 8
       helm-ff-file-name-history-use-recentf t)
@@ -94,63 +59,63 @@
 (projectile-global-mode)
 
 (use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
-          treemacs-deferred-git-apply-delay   0.5
-          treemacs-display-in-side-window     t
-          treemacs-file-event-delay           5000
-          treemacs-file-follow-delay          0.2
-          treemacs-follow-after-init          t
-          treemacs-follow-recenter-distance   0.1
-          treemacs-git-command-pipe           ""
-          treemacs-goto-tag-strategy          'refetch-index
-          treemacs-indentation                2
-          treemacs-indentation-string         " "
-          treemacs-is-never-other-window      nil
-          treemacs-max-git-entries            5000
-          treemacs-no-png-images              nil
-          treemacs-project-follow-cleanup     nil
-          treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-recenter-after-file-follow nil
-          treemacs-recenter-after-tag-follow  nil
-          treemacs-show-cursor                nil
-          treemacs-show-hidden-files          t
-          treemacs-silent-filewatch           nil
-          treemacs-silent-refresh             nil
-          treemacs-sorting                    'alphabetic-desc
-          treemacs-space-between-root-nodes   t
-          treemacs-tag-follow-cleanup         t
-          treemacs-tag-follow-delay           1.5
-          treemacs-width                      40)
+    :ensure t
+    :defer t
+    :init
+    (with-eval-after-load 'winum
+      (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+    :config
+    (progn
+      (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+            treemacs-deferred-git-apply-delay   0.5
+            treemacs-display-in-side-window     t
+            treemacs-file-event-delay           5000
+            treemacs-file-follow-delay          0.2
+            treemacs-follow-after-init          t
+            treemacs-follow-recenter-distance   0.1
+            treemacs-git-command-pipe           ""
+            treemacs-goto-tag-strategy          'refetch-index
+            treemacs-indentation                2
+            treemacs-indentation-string         " "
+            treemacs-is-never-other-window      nil
+            treemacs-max-git-entries            5000
+            treemacs-no-png-images              nil
+            treemacs-project-follow-cleanup     nil
+            treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+            treemacs-recenter-after-file-follow nil
+            treemacs-recenter-after-tag-follow  nil
+            treemacs-show-cursor                nil
+            treemacs-show-hidden-files          t
+            treemacs-silent-filewatch           nil
+            treemacs-silent-refresh             nil
+            treemacs-sorting                    'alphabetic-desc
+            treemacs-space-between-root-nodes   t
+            treemacs-tag-follow-cleanup         t
+            treemacs-tag-follow-delay           1.5
+            treemacs-width                      40)
 
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
+      ;; The default width and height of the icons is 22 pixels. If you are
+      ;; using a Hi-DPI display, uncomment this to double the icon size.
+      ;;(treemacs-resize-icons 44)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  :bind
-  (:map global-map
-	([f8] . treemacs)
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+      (treemacs-follow-mode t)
+      (treemacs-filewatch-mode t)
+      (treemacs-fringe-indicator-mode t)
+      (pcase (cons (not (null (executable-find "git")))
+                   (not (null (executable-find "python3"))))
+	(`(t . t)
+	 (treemacs-git-mode 'deferred))
+	(`(t . _)
+	 (treemacs-git-mode 'simple))))
+    :bind
+    (:map global-map
+	  ([f8] . treemacs)
+          ("M-0"       . treemacs-select-window)
+          ("C-x t 1"   . treemacs-delete-other-windows)
+          ("C-x t t"   . treemacs)
+          ("C-x t B"   . treemacs-bookmark)
+          ("C-x t C-t" . treemacs-find-file)
+          ("C-x t M-t" . treemacs-find-tag)))
 
 
 (use-package treemacs-projectile
@@ -181,12 +146,10 @@
       (let ((kill-buffer-query-functions nil)) (kill-buffer "*Python*")))
   (elpy-shell-send-region-or-buffer))
 
+;; ToDo only for python-map:
 (global-set-key (kbd "C-c C-x C-c") 'my-restart-python-console)
 (global-set-key (kbd "C->") 'python-indent-shift-right)
 (global-set-key (kbd "C-<") 'python-indent-shift-left)
-
-
-
 
 (set-language-environment "UTF-8")
 (elpy-enable)
@@ -223,6 +186,7 @@
 
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+;; (add-hook 'clojure-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'prog-mode-hook 'paredit-everywhere-mode)
 (add-hook 'python-mode-hook 'elpy-mode)
@@ -230,6 +194,8 @@
 
 
 ;; (setq cider-eldoc-display-for-symbol-at-point t)
+;; (define-key cider-repl-mode-map (kbd "C-c M-b") 'cider-repl-clear-buffer)
+
 
 (defun move-line-up ()
   "Move up the current line."
@@ -258,6 +224,7 @@
 (global-set-key (kbd "M-o") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 (setq aw-background nil)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
